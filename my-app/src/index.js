0696a14b7504da944716a4bb3c53403e04fe6e14
ben harrington <harringtonben@icloud.com>
2018-03-04 12:52:40 -0600

    created first react component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {render} from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import StorePicker from './Components/StorePicker';
 
-alert("YEAH BOI REACT!!");
+render(<StorePicker/>, document.getElementById('root'));
