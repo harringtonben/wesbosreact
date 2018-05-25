ea7c0edc8ed59d843002fb6a66149d3373a31d26
ben harrington <harringtonben@icloud.com>
2018-05-25 16:57:09 -0500

    finished basic setup and making first component

--- my-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import './css/style.css';
-import App from './Components/app'; 
-import StorePicker from './Components/StorePicker'; 
-
-render(<App/>, document.getElementById('root'));
